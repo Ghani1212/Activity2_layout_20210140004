7ce64b8f4ff71b77fac7133d0e0737b8fe111592
M S I <ghani.zumar.ft21@mail.umy.ac.id>
2023-10-03 11:52:51 +0700

    done

---
+++ app/src/main/java/com/example/praktikum_1/Frame_Layout.kt
@@ -0,0 +1,11 @@
+package com.example.praktikum_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Frame_Layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_layout)
+    }
+}
